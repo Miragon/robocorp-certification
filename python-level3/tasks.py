ec69c6988781e00d841ba14d7e44a24530abe891
peterhnm <peter.hnm@gmx.de>
2023-12-19 12:53:52 +0100

    feat(level3): update python-level3

--- python-level3/tasks.py
+++ python-level3/tasks.py
@@ -1,5 +1,10 @@
+import requests
+from typing import List
+
 from robocorp import workitems
 from robocorp.tasks import task
+from robocorp.workitems import ExceptionType
+
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables, Table
@@ -9,6 +14,12 @@ json = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# JSON data keys
+COUNTRY_KEY = "SpatialDim"
+YEAR_KEY = "TimeDim"
+RATE_KEY = "NumericValue"
+GENDER_KEY = "Dim1"
 
 
 @task
@@ -25,7 +36,9 @@ def produce_traffic_data():
     )
     traffic_data = load_traffic_data_as_table()
     filtered_traffic_data = filter_and_sort_traffic_data(traffic_data)
-    table.write_table_to_csv(filtered_traffic_data, "output/traffic.csv")
+    filtered_traffic_data = get_latest_data_by_country(filtered_traffic_data)
+    payloads = create_work_item_payloads(filtered_traffic_data)
+    save_work_item_payloads(payloads)
 
 
 @task
@@ -35,6 +48,7 @@ def consume_traffic_data():
     Produce traffic data work items.
     """
     print("consume...")
+    process_traffic_data()
 
 
 def load_traffic_data_as_table() -> Table:
@@ -44,12 +58,65 @@ def load_traffic_data_as_table() -> Table:
 
 
 def filter_and_sort_traffic_data(traffic_data: Table) -> Table:
-    rate_key = "NumericValue"
     max_rate = 5.0
-    gender_key = "Dim1"
     both_genders = "BTSX"
-    year_key = "TimeDim"
-    table.filter_table_by_column(traffic_data, rate_key, "<", max_rate)
-    table.filter_table_by_column(traffic_data, gender_key, "==", both_genders)
-    table.sort_table_by_column(traffic_data, year_key, False)
+    table.filter_table_by_column(traffic_data, RATE_KEY, "<", max_rate)
+    table.filter_table_by_column(traffic_data, GENDER_KEY, "==", both_genders)
+    table.sort_table_by_column(traffic_data, YEAR_KEY, False)
     return traffic_data
+
+
+def get_latest_data_by_country(traffic_data: Table) -> list[dict | list]:
+    country_key = COUNTRY_KEY
+    data = table.group_table_by_column(traffic_data, country_key)
+    latest_data_by_country = []
+    for group in data:
+        first_row = table.pop_table_row(group)
+        latest_data_by_country.append(first_row)
+
+    return latest_data_by_country
+
+
+def create_work_item_payloads(traffic_data: list[dict | list]) -> List[dict]:
+    payloads = []
+    for row in traffic_data:
+        payload = dict(
+            country=row[COUNTRY_KEY],
+            year=row[YEAR_KEY],
+            rate=row[RATE_KEY],
+        )
+        payloads.append(payload)
+    return payloads
+
+
+def save_work_item_payloads(payloads: List[dict]):
+    for payload in payloads:
+        variables = dict(traffic_data=payload)
+        workitems.outputs.create(variables)
+
+
+def process_traffic_data():
+    for item in workitems.inputs:
+        traffic_data = item.payload["traffic_data"]
+        if len(traffic_data["country"]) == 3:
+            status, return_json = post_traffic_data_to_sales_system(traffic_data)
+            if status == 200:
+                item.done()
+            else:
+                item.fail(
+                    exception_type=ExceptionType.APPLICATION,
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"],
+                )
+        else:
+            item.fail(
+                exception_type=ExceptionType.BUSINESS,
+                code="INVALID_TRAFFIC_DATA",
+                message=item.payload,
+            )
+
+
+def post_traffic_data_to_sales_system(traffic_data: dict) -> (int, dict):
+    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
+    response = requests.post(url, json=traffic_data)
+    return response.status_code, response.json()
